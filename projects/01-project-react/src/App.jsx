136d97dfe19d37c1812608bf693371ed657e9295
gfCrova <giancrova.cl5@gmail.com>
2023-02-02 11:12:43 -0300

    modified h1 and count

--- projects/01-project-react/src/App.jsx
+++ projects/01-project-react/src/App.jsx
@@ -18,11 +18,11 @@ function App() {
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Contador en: {count}
         </button>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Click en los logos de Vite y React para más información
       </p>
     </div>
   )
